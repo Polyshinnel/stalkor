128183bbf85eebe027ec0d9a2092f4a570b85e8c
Polyshinnel <polyshinnel@gmail.com>
2022-09-02 16:05:52 +0300

    add parser command

--- bootstrap/console.php
+++ bootstrap/console.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Jobs\ParseCategory;
 use App\Jobs\TestJob;
 
 return [
     'console' => [
         'commands' => [
-            'Название комманды' => TestJob::class
+            'Название комманды' => TestJob::class,
+            'ParseCategory' => ParseCategory::class
         ]
     ]
 ];
--- src/Jobs/ParseCategory.php
+++ src/Jobs/ParseCategory.php
@@ -4,6 +4,7 @@
 namespace App\Jobs;
 
 
+use simplehtmldom\HtmlWeb;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,6 +20,22 @@ class ParseCategory extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output) : int
     {
+        $client = new HtmlWeb();
+        $html = $client->load('https://mc.ru/products');
+
+        $linkArr = [];
+        $rowLinksObj = $html->find('.productsMenuBlock h3 a');
+
+        foreach ($rowLinksObj as $item)
+        {
+            $catArr = [
+                'name' => $item->plaintext,
+                'link' => $item->href
+            ];
+            $linkArr[] = $catArr;
+        }
+
+        print_r($linkArr);
 
         return Command::SUCCESS;
     }
